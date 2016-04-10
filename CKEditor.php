7fa8d1bbb4372c1534951e29423fe8e4e187ef22
Loveorigami <loveorigami@mail.ru>
2016-04-10 16:33:13 +0300

    all presets

--- CKEditor.php
+++ CKEditor.php
@@ -14,7 +14,8 @@ use yii\web\View;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 
-class CKEditor extends InputWidget{
+class CKEditor extends InputWidget
+{
     public $editorOptions = [];
     public $containerOptions = [];
     private $_inline = false;
@@ -29,24 +30,25 @@ class CKEditor extends InputWidget{
         }
 
         if (array_key_exists('preset', $this->editorOptions)) {
-            if($this->editorOptions['preset'] == 'basic'){
+            if ($this->editorOptions['preset'] == 'basic') {
                 $this->presetBasic();
-            }elseif($this->editorOptions['preset'] == 'standard'){
+            } elseif ($this->editorOptions['preset'] == 'standard') {
                 $this->presetStandard();
-            }elseif($this->editorOptions['preset'] == 'full'){
+            } elseif ($this->editorOptions['preset'] == 'full') {
                 $this->presetFull();
             }
             unset($this->editorOptions['preset']);
         }
 
-        if($this->_inline && !isset($this->editorOptions['height']))
+        if ($this->_inline && !isset($this->editorOptions['height']))
             $this->editorOptions['height'] = 100;
 
-        if($this->_inline && !isset($this->containerOptions['id']))
-            $this->containerOptions['id'] = $this->id.'_inline';
+        if ($this->_inline && !isset($this->containerOptions['id']))
+            $this->containerOptions['id'] = $this->id . '_inline';
     }
 
-    private function presetBasic(){
+    private function presetBasic()
+    {
         $options['height'] = 100;
 
         $options['toolbarGroups'] = [
@@ -54,8 +56,11 @@ class CKEditor extends InputWidget{
             ['name' => 'basicstyles', 'groups' => ['basicstyles', 'cleanup']],
             ['name' => 'colors'],
             ['name' => 'links', 'groups' => ['links', 'insert']],
-            ['name' => 'others','groups' => ['others', 'about']],
+            ['name' => 'others', 'groups' => ['others', 'about']],
         ];
+        
+        $options['extraPlugins'] = 'codemirror';
+
         $options['removeButtons'] = 'Subscript,Superscript,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe';
         $options['removePlugins'] = 'elementspath';
         $options['resize_enabled'] = false;
@@ -64,11 +69,12 @@ class CKEditor extends InputWidget{
         $this->editorOptions = ArrayHelper::merge($options, $this->editorOptions);
     }
 
-    private function presetStandard(){
+    private function presetStandard()
+    {
         $options['height'] = 300;
 
         $options['toolbarGroups'] = [
-            ['name' => 'clipboard', 'groups' => ['mode','undo', 'selection', 'clipboard','doctools']],
+            ['name' => 'clipboard', 'groups' => ['mode', 'undo', 'selection', 'clipboard', 'doctools']],
             ['name' => 'editing', 'groups' => ['tools', 'about']],
             '/',
             ['name' => 'paragraph', 'groups' => ['templates', 'list', 'indent', 'align']],
@@ -82,7 +88,9 @@ class CKEditor extends InputWidget{
 
         $options['removeButtons'] = 'Smiley,Iframe';
 
-        if($this->_inline){
+        $options['extraPlugins'] = 'codemirror';
+
+        if ($this->_inline) {
             $options['extraPlugins'] = 'sourcedialog';
             $options['removePlugins'] = 'sourcearea';
         }
@@ -91,12 +99,12 @@ class CKEditor extends InputWidget{
     }
 
 
-
-    private function presetFull(){
+    private function presetFull()
+    {
         $options['height'] = 400;
 
         $options['toolbarGroups'] = [
-            ['name' => 'clipboard', 'groups' => ['mode','undo', 'selection', 'clipboard', 'doctools']],
+            ['name' => 'clipboard', 'groups' => ['mode', 'undo', 'selection', 'clipboard', 'doctools']],
             ['name' => 'editing', 'groups' => ['find', 'spellchecker', 'tools', 'about']],
 
             ['name' => 'paragraph', 'groups' => ['templates', 'list', 'indent', 'align']],
@@ -107,12 +115,14 @@ class CKEditor extends InputWidget{
             ['name' => 'styles'],
             ['name' => 'blocks'],
 
-            ['name' => 'basicstyles', 'groups' => ['basicstyles', 'colors','cleanup']],
+            ['name' => 'basicstyles', 'groups' => ['basicstyles', 'colors', 'cleanup']],
             ['name' => 'links', 'groups' => ['links', 'insert']],
             ['name' => 'others'],
         ];
+
         $options['extraPlugins'] = 'codemirror';
-        if($this->_inline){
+
+        if ($this->_inline) {
             $options['extraPlugins'] = 'sourcedialog';
             $options['removePlugins'] = 'sourcearea';
         }
@@ -159,7 +169,7 @@ mihaildev.ckEditor = {
 };
 JS
             , View::POS_READY, 'mihaildev-ckeditor'
-);
+        );
 
         echo Html::beginTag('div', $this->containerOptions);
         if ($this->hasModel()) {
@@ -169,30 +179,29 @@ JS
         }
 
         echo Html::endTag('div');
-		$js = [
-			'mihaildev.ckEditor.registerOnChange('.Json::encode($this->options['id']).');'
-		];
+        $js = [
+            'mihaildev.ckEditor.registerOnChange(' . Json::encode($this->options['id']) . ');'
+        ];
 
-		if(isset($this->editorOptions['filebrowserUploadUrl']))
-			$js[] = "mihaildev.ckEditor.registerCsrf();";
+        if (isset($this->editorOptions['filebrowserUploadUrl']))
+            $js[] = "mihaildev.ckEditor.registerCsrf();";
 
-		if(!isset($this->editorOptions['on']['instanceReady']))
-			$this->editorOptions['on']['instanceReady'] = new JsExpression("function( ev ){".implode(' ', $js)."}");
+        if (!isset($this->editorOptions['on']['instanceReady']))
+            $this->editorOptions['on']['instanceReady'] = new JsExpression("function( ev ){" . implode(' ', $js) . "}");
 
-        if($this->_inline){
-            $JavaScript = "CKEDITOR.inline(";
+        $JavaScript = "CKEDITOR.plugins.addExternal('codemirror', '$bundle->baseUrl/codemirror/');";
+
+        if ($this->_inline) {
+            $JavaScript .= "CKEDITOR.inline(";
             $JavaScript .= Json::encode($this->options['id']);
-            $JavaScript .= empty($this->editorOptions) ? '' : ', '.Json::encode($this->editorOptions);
+            $JavaScript .= empty($this->editorOptions) ? '' : ', ' . Json::encode($this->editorOptions);
             $JavaScript .= ");";
-
             $this->getView()->registerJs($JavaScript, View::POS_END);
-            $this->getView()->registerCss('#'.$this->containerOptions['id'].', #'.$this->containerOptions['id'].' .cke_textarea_inline{height: '.$this->editorOptions['height'].'px;}');
-        }else{
-
-            $JavaScript = "CKEDITOR.plugins.addExternal('codemirror', '$bundle->baseUrl/codemirror/');";
+            $this->getView()->registerCss('#' . $this->containerOptions['id'] . ', #' . $this->containerOptions['id'] . ' .cke_textarea_inline{height: ' . $this->editorOptions['height'] . 'px;}');
+        } else {
             $JavaScript .= "CKEDITOR.replace(";
             $JavaScript .= Json::encode($this->options['id']);
-            $JavaScript .= empty($this->editorOptions) ? '' : ', '.Json::encode($this->editorOptions);
+            $JavaScript .= empty($this->editorOptions) ? '' : ', ' . Json::encode($this->editorOptions);
             $JavaScript .= ");";
 
             $this->getView()->registerJs($JavaScript, View::POS_END);
